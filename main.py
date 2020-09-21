8a00e1194fd062af8b1136a584201a651e4f7cdb
Amanikashema <61458926+Amanikashema@users.noreply.github.com>
2020-09-21 11:41:22 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+#Task1
+print("Hello world")
+
+#Task2
+import math  #importing maths module
+print("Solving the Area,Diameter and Circumference of a circle.")
+radius = float(input("Enter the radius of the circle: "))
+area = radius**2 * math.pi   #calculation of area circle
+print("The Area of the circle is equal to =", area,"meters")   #printing results
+
+#Task3
+diameter = 2 * radius
+print("The diameter is equal to =",diameter,"meters")
+circumference = 2*math.pi*radius
+print("The circumference is equal to =",circumference,"meters")
+
+
